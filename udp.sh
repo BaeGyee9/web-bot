8602c1d0679804a931fc99b6ba4dc85bb4b6ba27
BaeGyee9 <zawtunwaiz@gmail.com>
2025-12-05 03:19:04 +0700

    Update udp.sh

--- udp.sh
+++ udp.sh
@@ -775,7 +775,7 @@ ufw allow 1:65535/udp >/dev/null 2>&1 || true
 # ufw allow 22/tcp >/dev/null 2>&1 || true
 # ufw allow 5667/udp >/dev/null 2>&1 || true
 # ufw allow 6000:19999/udp >/dev/null 2>&1 || true
-# ufw allow 19432/tcp >/dev/null 2>&1 || true
+# ufw allow 1943/tcp >/dev/null 2>&1 || true
 # ufw allow 8081/tcp >/dev/null 2>&1 || true
 ufw --force enable >/dev/null 2>&1 || true
 
@@ -801,7 +801,7 @@ systemctl restart zivpn.service
 # ===== Completion Message (ORIGINAL CODE) =====
 IP=$(hostname -I | awk '{print $1}')
 echo -e "\n$LINE\n${G}✅ ZIVPN Enterprise Edition Completed!${Z}"
-echo -e "${C}🌐 WEB PANEL:${Z} ${Y}http://$IP:19432${Z}"
+echo -e "${C}🌐 WEB PANEL:${Z} ${Y}http://$IP:1943${Z}"
 # echo -e "  ${C}Login:${Z} ${Y}$WEB_USER / $WEB_PASS${Z}"
 echo -e "\n${G}🔐 LOGIN CREDENTIALS${Z}"
 echo -e "  ${Y}• Username:${Z} ${Y}$WEB_USER${Z}"
